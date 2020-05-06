40f98b4b6164d7a0ebfa8184c68c9f521bc6de08
Carlo <carlo.panzi@gmail.com>
2020-05-06 16:39:43 +0200

    logging and validation

--- src/routes/tracking.js
+++ src/routes/tracking.js
@@ -6,7 +6,13 @@ module.exports = [
     path: `/notification`,
 
     handler: (req, res) => {
+      console.log("--- new post", req.body);
+
       const { channel_id, ...rest } = req.body;
+
+      if (!channel_id) {
+        res.send(400, "Channel is empty");
+      }
 
       clients.pushMessage(channel_id, rest);
 
